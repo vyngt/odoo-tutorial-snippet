eb195fb3e7f6f2accfe85da3acf49fcad1dd243b
TheVy <ntvy2k@gmail.com>
2023-02-27 08:50:52 +0700

    basic snippet

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Tutorial Snippet",
+    "summary": """
+        tutorial snippets""",
+    "description": """
+        ok
+    """,
+    "author": "vyngt",
+    "website": "https://github.com/vyngt",
+    "category": "Uncategorized",
+    "version": "16.0.1.0.0",
+    "depends": ["base", "website"],
+    "data": [
+        # 'security/ir.model.access.csv',
+        "views/snippets/v_block.xml",
+        "views/snippets/snippets.xml",
+    ],
+}
